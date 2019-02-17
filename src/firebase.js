a6c0ac0d4ce173184b9f7515b85b86f953814741
Phillip Darby <darby.phillip@gmail.com>
2019-02-16 21:18:46 -0500

    A

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
-var config = {
-  apiKey: 'AIzaSyCOxLNgd2VGaNa5tZ8wN_Fqdl9sKUBaL6Y',
-  authDomain: 'mis-chat-c3988.firebaseapp.com',
-  databaseURL: 'https://mis-chat-c3988.firebaseio.com',
-  projectId: 'mis-chat-c3988',
-  storageBucket: 'mis-chat-c3988.appspot.com',
-  messagingSenderId: '809652115000'
-};
+//add config from firebase
 
 firebase.initializeApp(config);
 
