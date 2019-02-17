306b0502690a15df0957bbaf9370ac60cd1c9284
Phillip Darby <darby.phillip@gmail.com>
2019-02-16 21:20:44 -0500

    modified git ignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
-//add config from firebase
+var config = {
+  apiKey: 'AIzaSyCOxLNgd2VGaNa5tZ8wN_Fqdl9sKUBaL6Y',
+  authDomain: 'mis-chat-c3988.firebaseapp.com',
+  databaseURL: 'https://mis-chat-c3988.firebaseio.com',
+  projectId: 'mis-chat-c3988',
+  storageBucket: 'mis-chat-c3988.appspot.com',
+  messagingSenderId: '809652115000'
+};
 
 firebase.initializeApp(config);
 
